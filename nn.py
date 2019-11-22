22a70b9011f07e39f6df9cfe89d665cef6a58790
danielefongo <danielefongo@gmail.com>
2019-11-22 11:26:01 +0100

    separate functions

--- nn.py
+++ nn.py
@@ -12,14 +12,28 @@ X = np.column_stack((X1, X2, np.ones(X1.size)))
 W = np.zeros((X.shape[1], Y.shape[1]))
 
 
-def predict(x, w):
+def weighted_sum(w, x):
     return np.matmul(x, w)
+
+def activate(input):
+    return input
+
+def derivative_activate(output):
+    return 1
+
+def predict(x, w):
+    return activate(weighted_sum(w, x))
 
 def calculate_loss(y, out):
     return np.power(out - y, 2).mean()
 
+def derivative_loss(y, out):
+    return (2 * (out - y))
+
 def calculate_gradient(x, y, out):
-    return (2 * np.matmul(x.T, out - y)) / x.shape[0]
+    d_loss = derivative_loss(y, out)
+    d_w = d_loss * derivative_activate(out)
+    return np.matmul(x.T, d_w) / x.shape[0]
 
 for i in np.arange(1, 1000):
     out = predict(X, W)
