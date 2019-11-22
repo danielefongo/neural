b9c250039fb4656d2d03ef1ae761f46d92bb795e
danielefongo <danielefongo@gmail.com>
2019-11-22 11:38:09 +0100

    create loss and layer classes

--- nn.py
+++ nn.py
@@ -1,48 +1,59 @@
 import numpy as np
 
-
 X1 = np.random.random_integers(1, 10, 3000)
 X2 = np.random.random_integers(1, 10, 3000)
 Y = X1 + X2
 
-#Train
+# Train
 learning_rate = 0.001
 Y = np.expand_dims(Y, 1)
 X = np.column_stack((X1, X2, np.ones(X1.size)))
-W = np.zeros((X.shape[1], Y.shape[1]))
 
-class Activation(): # Linear
+def weighted_sum(w, x):
+    return np.matmul(x, w)
+
+
+class Activation():  # Linear
     def activate(self, input):
         return input
 
     def derivative(self, output):
         return 1
 
-activation = Activation()
 
-def weighted_sum(w, x):
-    return np.matmul(x, w)
+class Loss():
+    def calculate(self, out, y):
+        return np.power(out - y, 2).mean()
 
-def predict(x, w):
-    return activation.activate(weighted_sum(w, x))
+    def derivative(self, out, y):
+        return (2 * (out - y))
 
-def calculate_loss(y, out):
-    return np.power(out - y, 2).mean()
 
-def derivative_loss(y, out):
-    return (2 * (out - y))
+class Layer():
+    def __init__(self, input_size, output_size, activation):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.activation = activation
+        self.weights = np.zeros((input_size, output_size))
 
-def calculate_gradient(x, y, out):
-    d_loss = derivative_loss(y, out)
-    d_w = d_loss * activation.derivative(out)
-    return np.matmul(x.T, d_w) / x.shape[0]
+    def predict(self, x):
+        return activation_function.activate(weighted_sum(self.weights, x))
+
+    def update(self, x, out, d_next):
+        d_w = d_next * activation_function.derivative(out)
+        gradient = np.matmul(x.T, d_w) / x.shape[0]
+        self.weights -= gradient * learning_rate
+
+activation_function = Activation()
+loss_function = Loss()
+layer = Layer(X.shape[1], Y.shape[1], activation_function)
 
 for i in np.arange(1, 1000):
-    out = predict(X, W)
-    loss = calculate_loss(Y, out)
+    out = layer.predict(X)
+    loss = loss_function.calculate(out, Y)
+    d_loss = loss_function.derivative(out, Y)
     print(loss)
-    gradient = calculate_gradient(X, Y, out)
-    W -= gradient * learning_rate
+    layer.update(X, out, d_loss)
 
 sample_range = np.arange(10)
 print(out[sample_range])
