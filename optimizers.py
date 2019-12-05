cfebe7384352e0785a0a35b2bf4ec4f9fdb45a92
danielefongo <danielefongo@gmail.com>
2019-12-05 09:01:15 +0100

    minor changes on optimizers

--- optimizers.py
+++ optimizers.py
@@ -9,12 +9,12 @@ class Optimizer:
     def set_iteration(self, iteration: int):
         self.iteration = iteration
 
-    def on(self, layer, inputs, d_loss):
+    def on(self, unit, inputs: np.ndarray, d_loss: np.ndarray):
         raise NotImplementedError("Should have implemented this")
 
 
 class GradientDescent(Optimizer):
-    def on(self, layer, inputs, d_loss):
+    def on(self, unit, inputs: np.ndarray, d_loss: np.ndarray):
         gradient = (np.matmul(d_loss.T, inputs) / inputs.shape[0])
         return gradient.T * self.learning_rate
 
@@ -27,27 +27,29 @@ class Adam(GradientDescent):
         self.epsilon = epsilon
         self.units = {}
 
-    def on(self, layer, inputs, d_loss):
-        if layer not in self.units:
-            self.units[layer] = dict(mean = 0, variance = 0)
+    def on(self, unit, inputs: np.ndarray, d_loss: np.ndarray):
+        if unit not in self.units:
+            self.units[unit] = dict(mean = 0, variance = 0)
 
-        gradient = super().on(layer, inputs, d_loss)
+        gradient = super().on(unit, inputs, d_loss)
 
-        mean, variance = self._mean_and_variance(layer, gradient)
-        self._update(layer, mean, variance)
+        mean, variance = self._mean_and_variance(unit, gradient)
+        self._update(unit, mean, variance)
 
         mean_hat = mean / (1 - np.power(self.beta1, self.iteration))
         variance_hat = variance / (1 - np.power(self.beta2, self.iteration))
 
         return (self.learning_rate * mean_hat) / (np.sqrt(variance_hat) + self.epsilon)
 
-    def _update(self, layer, mean, variance):
-        self.units[layer]["mean"] = mean
-        self.units[layer]["variance"] = variance
+    def _update(self, unit, mean, variance):
+        self.units[unit]["mean"] = mean
+        self.units[unit]["variance"] = variance
 
-    def _mean_and_variance(self, layer, gradient):
-        mean = self.units[layer]["mean"]
-        variance = self.units[layer]["variance"]
+    def _mean_and_variance(self, unit, gradient):
+        mean = self.units[unit]["mean"]
+        variance = self.units[unit]["variance"]
+
         mean = self.beta1 * mean + (1 - self.beta1) * gradient
         variance = self.beta2 * variance + (1 - self.beta2) * np.power(gradient, 2)
+
         return mean, variance
