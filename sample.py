7ec86b7b9be5290df10f35e02eb564caa2817313
danielefongo <danielefongo@gmail.com>
2020-02-04 17:15:02 +0100

    change sample file

--- sample.py
+++ sample.py
@@ -1,36 +1,36 @@
 import numpy as np
 
 from neural.activations import Softmax, Tanh
-from neural.layers import Layer
+from neural.layers import Layer, SimpleRNN
 from neural.losses import CrossEntropy
 from neural.networks import Network
-from neural.optimizers import Adam
+from neural.optimizers import GradientDescent
 
 # Load RANDOM data
-X = np.random.random_integers(1, 30, 3000)
-X = np.reshape(X, (-1, 2))
-Y1 = X[:, 0] > X[:, 1]
-Y2 = X[:, 0] <= X[:, 1]
+X = np.random.random_integers(1, 30, (3000, 2, 2))
+Y1 = X[:, -1, 0] > X[:, -1, 1]
+Y2 = X[:, -1, 0] <= X[:, -1, 1]
 Y = np.stack((Y1, Y2), 1) * 1.0
 
 # Train
 epochs = 5
 batch_size = 32
 learning_rate = 0.001
-optimizer = Adam(learning_rate)
 
 input_features = X.shape[-1]
 output_features = Y.shape[-1]
 
 network = Network()
-network.add(Layer(100, Tanh()))
+network.add(SimpleRNN(100, 2, Tanh()))
 network.add(Layer(Y.shape[-1], Softmax()))
-network.train(X, Y, batch_size, epochs, CrossEntropy(), optimizer, shuffle=False)
+network.train(X, Y, batch_size, epochs, CrossEntropy(), GradientDescent(learning_rate), shuffle=False)
+
 config, variables = network.export()
 
 new_network = Network()
 new_network.use(config, variables)
 
-new_network.train(X, Y, batch_size, epochs, CrossEntropy(), optimizer, shuffle=False)
+new_network.train(X, Y, batch_size, epochs, CrossEntropy(), GradientDescent(learning_rate), shuffle=False)
 print(new_network.evaluate(X)[:3])
 print(Y[:3])
+
